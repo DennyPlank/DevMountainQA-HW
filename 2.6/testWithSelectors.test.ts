4081642e550f906468f465283fe4aa29ccfb9a82
Dennis Plank <DennyMouzon@gmail.com>
2022-11-15 22:15:18 -0800

    added a few tests and got it working

--- 2.6/testWithSelectors.test.ts
+++ 2.6/testWithSelectors.test.ts
@@ -1,4 +1,4 @@
-import {Builder, By, Capabilities, WebDriver} from "selenium-webdriver"
+import {Builder, By, Capabilities, WebDriver, until} from "selenium-webdriver"
 
 const chromedriver = require("chromedriver")
 
@@ -10,23 +10,29 @@ describe("Filling in the blanks", () => {
     });
     afterAll(async () => {
         await driver.quit();
+        await driver.sleep(5000)
     })
 
-    const hdrInput: By = By. //fill in the blank
-    const mkeInput: By = By. //fill in the blank
-    const oaiInput: By = By. //fill in the blank
-    const nameInput: By = By. //fill in the blank
-    const clrBtn: By = By. //fill in blank 
-    const submitBtn: By = By. //fill in blank
-    const errorMsg: By = By. // fill in blank 
-
-    test("filling in the blanks for real", () => {
+    const hdrInput: By = By.xpath("//input[@name='hdrInput']") //fill in the blank
+    const mkeInput: By = By.xpath("//input[@name='mkeInput']") //fill in the blank
+    const oaiInput: By = By.xpath("//input[@name='oriInput']") //fill in the blank
+    const nameInput: By = By.xpath("//input[@name='namInput']") //fill in the blank
+    const clrBtn: By = By.xpath("//button[@id='clearBtn']") //fill in blank 
+    const submitBtn: By = By.xpath("//button[@id='saveBtn']") //fill in blank
+    const errorMsg: By = By.xpath("//p[@id='validHeader']") // fill in blank 
+    
+    console.log(errorMsg)
+    
+    test("filling in the blanks for real", async () => {
         await driver.findElement(hdrInput).sendKeys("Change this")
         await driver.findElement(mkeInput).sendKeys("change this")
         await driver.findElement(oaiInput).sendKeys("change this")
         await driver.findElement(nameInput).sendKeys("change this")
         await driver.findElement(submitBtn).click()
-        expect(errorMsg).toContain("Errors Received:")
+
+        await driver.findElement(errorMsg) 
+        let errorMesg = await driver.findElement(errorMsg).getText()
+        expect(errorMesg).toContain("Errors Received")
         await driver.findElement(clrBtn).click()
         
     })
